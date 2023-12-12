3b9abe11ef2cbd8002b524fcb3841ef3585f1465
Guodong Jin <guod.jin@gmail.com>
2023-12-11 23:03:21 -0500

    try robinhood

--- examples/cpp/main.cpp
+++ examples/cpp/main.cpp
@@ -1,16 +1,38 @@
 #include <iostream>
-
 #include <random>
 
 #include "common/timer.h"
+#include "robinhood.h"
 #include "storage/index/hash_index_builder.h"
 
 using namespace kuzu::common;
 using namespace kuzu::storage;
 
-int main() {
-    auto pkIndex = std::make_unique<PrimaryKeyIndexBuilder>("./test.hindex", *LogicalType::INT64());
+void testRobinHood(const std::vector<int64_t>& keys) {
+    auto robinHoodMap = std::make_unique<robin_hood::unordered_map<int64_t, int64_t>>();
+    Timer timer;
+    timer.start();
+    for (int64_t i = 0; i < keys.size(); i++) {
+        robinHoodMap->insert({keys[i], i});
+    }
+    auto timeSpent = timer.getElapsedTimeInMS();
+    std::cout << "Time spent: " << timeSpent << " ms" << std::endl;
+}
 
+void testHashIndex(const std::vector<int64_t>& keys) {
+    auto pkIndex = std::make_unique<PrimaryKeyIndexBuilder>("./test.hindex", *LogicalType::INT64());
+    pkIndex->bulkReserve(keys.size());
+    Timer timer;
+    timer.start();
+    for (int64_t i = 0; i < keys.size(); i++) {
+        pkIndex->append(keys[i], i);
+    }
+    auto timeSpent = timer.getElapsedTimeInMS();
+    std::cout << "Time spent: " << timeSpent << " ms" << std::endl;
+    pkIndex->flush();
+}
+
+int main() {
     int64_t num = 100000000;
     std::vector<int64_t> keys;
     keys.reserve(num);
@@ -19,15 +41,6 @@ int main() {
     for (int64_t i = 0; i < num; i++) {
         keys.push_back(dist(rng));
     }
-
-    pkIndex->bulkReserve(num);
-    Timer timer;
-    timer.start();
-    for (int64_t i = 0; i < num; i++) {
-        pkIndex->append(keys[i], i);
-    }
-    auto timeSpent = timer.getElapsedTimeInMS();
-    std::cout << "Time spent: " << timeSpent << " ms" << std::endl;
-
-    pkIndex->flush();
+    testRobinHood(keys);
+//    testHashIndex(keys);
 }
